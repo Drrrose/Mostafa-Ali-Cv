0a950361e8e992946e012fbf6bbc25d420316c1d
Drrrose <https://Drrrose@github.com>
2025-11-29 16:38:07 +0200

    Setup GitHub Pages deployment

--- .output/server/chunks/build/app-styles.D1rZCRyd.mjs
+++
@@ -1,8 +0,0 @@
-const app_vue_vue_type_style_index_0_lang = "@media print{@page{margin:0;size:A4}body{background:#fff;-webkit-print-color-adjust:exact;print-color-adjust:exact}}";
-
-const appStyles_D1rZCRyd = [
-  app_vue_vue_type_style_index_0_lang
-];
-
-export { appStyles_D1rZCRyd as default };
-//# sourceMappingURL=app-styles.D1rZCRyd.mjs.map
