496e6a752fb198a4b28da77a7e191884d6413e33
Drrrose <https://Drrrose@github.com>
2025-12-14 20:48:20 +0200

    adjustments

--- app/app.vue
+++ app/app.vue
@@ -32,7 +32,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-primary shrink-0" viewBox="0 0 24 24" fill="currentColor">
                       <path fill-rule="evenodd" d="M12.516 2.17a.75.75 0 00-1.032 0 11.209 11.209 0 01-7.877 3.08.75.75 0 00-.722.515A12.74 12.74 0 002.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 00.374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 00-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08zm3.094 8.016a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
                     </svg>
-                    <span>Completed</span>
+                    <span>Exempted</span>
                   </div>
         
                   <!-- DOB -->
@@ -101,7 +101,7 @@
         <!-- Job 1 -->
         <article class="flex flex-col md:flex-row mb-4 print:mb-2">
           <div class="w-full md:w-[15%] text-sm font-semibold text-primary pt-1 md:whitespace-nowrap mb-1 md:mb-0">
-            <p>06/2024–present</p>
+            <p>06/2024–11/2025</p>
             <p class="text-primary">Cairo, Egypt</p>
           </div>
           <div class="w-full md:w-[85%] pl-0 md:pl-6">
@@ -124,8 +124,7 @@
         <!-- Job 2 -->
         <article class="flex flex-col md:flex-row">
            <div class="w-full md:w-[15%] text-sm font-semibold text-primary pt-1 md:whitespace-nowrap mb-1 md:mb-0">
-            <p>01/2024–</p>
-            <p>03/2024</p>
+            <p>01/2024–06/2024</p>
             <p class="text-primary">Cairo, Egypt</p>
           </div>
            <div class="w-full md:w-[85%] pl-0 md:pl-6">
@@ -155,7 +154,7 @@
 
         <article class="flex flex-col md:flex-row mb-4">
           <div class="w-full md:w-[15%] text-sm font-semibold text-primary pt-1 md:whitespace-nowrap mb-1 md:mb-0">
-            <p>10/2021 –</p><p>06/2025</p>
+            <p>10/2021 – 06/2025</p>
             <p><span class="border-b-2 ">Cairo</span>,Egypt</p>
           </div>
           <div class="w-full md:w-[85%] pl-0 md:pl-6">
